30e13f6e3e1c2b1f9238b762f403e19f67f69e5e
dougefresh <dchimento@gmail.com>
2026-03-28 17:16:00 +0000

    fix: lic

--- Formula/kiro-generator.rb
+++ Formula/kiro-generator.rb
@@ -1,10 +1,9 @@
 class KiroGenerator < Formula
   desc "Kiro Agent CLI configuration management"
   homepage "https://kiro-generator.io"
-  license "MIT"
-
   url "https://github.com/kiro-generator/kiro-generator/releases/download/v0.2.1/kg-arm64-darwin.tar.gz"
   sha256 "2e1db988420d62c17a830c8ede7806315c2430dbe7cf27570ab1ee25e1b7e50d"
+  license "MIT"
 
   def install
     bin.install "bin/kg"
